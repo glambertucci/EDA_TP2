af507f65515525ebf071e41bfa1c99ae49107453
Germán Bertachini <germanbertachini@outlook.com>
2018-03-13 11:53:17 -0300

    todo mergeado

--- TP2_EDA/TP2_EDA/Header files/mandelbrot.h
+++ TP2_EDA/TP2_EDA/Header files/mandelbrot.h
@@ -5,8 +5,6 @@
 #include <stdlib.h>
 #include <allegro5/allegro_color.h>
 #include "allegro5/allegro5.h"
-#define LIMIT 255
-#define OFFSET 50
 
 #ifndef _COORD_T 
 #define _COORD_T
@@ -16,10 +14,9 @@ typedef struct { //Estructura para uso de coordenadas.
 }Coord_t;
 #endif /* _COORD_T */
 
-/* Función principal de inicialización del display para la interfaz allegro, 
-recibe dos estructuras que contienen las coordenadas x e y de los puntos inicial y final
-entre los cuales definen el plano a analizar. Muestra en pantalla el resultado.
+/* Función principal para la creacón del fractal. recibe un puntero a la estructura del tipo infoType
+y el puntero a display. Muestra en pantalla el resultado.
 */
-int MandelAl(infoType * myData);
+void MandelAl(infoType* myData, ALLEGRO_DISPLAY* display);
 
 #endif /* MANDELBROT_H */
